bce842add9c64122791e91c5d3981be84c8041be
akkuman <akkumans@qq.com>
2021-09-28 17:53:49 +0800

    Update main.go
    fixed https://github.com/viodo/sonar-dingtalk-plugin/issues/2
    fix bug with project key

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+var addr = flag.String("addr", "0.0.0.0:9001", "输入监听地址")
+var token = flag.String("token", "", "输入sonarqube token")
+var httpClient = &http.Client{}
+
+func getMeasures(sonarUrl, projectKey interface{}) (*http.Response, error) {
+	url := fmt.Sprintf("%s/api/measures/search?projectKeys=%s&metricKeys=alert_status,bugs,reliability_rating,vulnerabilities,security_rating,code_smells,sqale_rating,duplicated_lines_density,coverage,ncloc,ncloc_language_distribution", sonarUrl, projectKey)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(*token, "")
+	resp, err := httpClient.Do(req)
+	return resp, err
+}
 
 // dingTalkHandler
 func dingTalkHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +37,8 @@ func dingTalkHandler(w http.ResponseWriter, r *http.Request) {
 	sonarUrl := sonarRsp["serverUrl"]
 	// 项目名称
 	projectName := sonarRsp["project"].(map[string]interface{})["name"]
+	// 项目key
+	projectKey := sonarRsp["project"].(map[string]interface{})["key"]
 	// 分支名称
 	branchName := sonarRsp["branch"].(map[string]interface{})["name"]
 	// sonar prop
@@ -29,8 +47,13 @@ func dingTalkHandler(w http.ResponseWriter, r *http.Request) {
 	var sendUrl, text, picUrl, messageUrl string
 
 	// get sonar info
-	resp, _ := http.Get(fmt.Sprintf("%s/api/measures/search?projectKeys=%s&metricKeys=alert_status,bugs,reliability_rating,vulnerabilities,security_rating,code_smells,sqale_rating,duplicated_lines_density,coverage,ncloc,ncloc_language_distribution",
-		sonarUrl, projectName))
+	resp, err := getMeasures(sonarUrl, projectKey)
+	if err != nil {
+		fmt.Printf("request measures error: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "request measures error: %v", err)
+		return
+	}
 	measuresObj := make(map[string]interface{})
 	if err := json.NewDecoder(resp.Body).Decode(&measuresObj); err == nil {
 		measures := measuresObj["measures"].([]interface{})
@@ -60,7 +83,7 @@ func dingTalkHandler(w http.ResponseWriter, r *http.Request) {
 		// 钉钉消息
 		sendUrl = fmt.Sprintf("https://oapi.dingtalk.com/robot/send?access_token=%s", accessToken)
 
-		messageUrl = fmt.Sprintf("%s/dashboard?id=%s", sonarUrl, projectName)
+		messageUrl = fmt.Sprintf("%s/dashboard?id=%s", sonarUrl, projectKey)
 
 		text = fmt.Sprintf("%s[%s]代码扫描结果：BUG数：%s个，漏洞数：%s个，异味数：%s个，覆盖率：%s%%，重复率：%s%%",
 			projectName, branchName, totalBugs, vulnerabilities, codeSmells, coverage, duplicatedLinesDensity)
@@ -82,8 +105,24 @@ func dingTalkHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
+
 func main() {
+	flag.Parse()
+	if !isFlagPassed("token") {
+		fmt.Println("token参数是必须的")
+		flag.Usage()
+		return
+	}
 	http.HandleFunc("/dingtalk", dingTalkHandler)
-	log.Println("Server started on port(s): 9001 (http)")
-	log.Fatal(http.ListenAndServe(":9001", nil))
+	log.Printf("Server started on port(s): %s (http)\n", *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
